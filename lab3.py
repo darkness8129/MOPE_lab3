04ad35cf77026ed3b404adc0b8a2401faa03f994
Yaroslaw <47916088+darkness8129@users.noreply.github.com>
2020-03-27 02:19:39 +0200

    aded scipy and m+1

--- lab3.py
+++ lab3.py
@@ -20,75 +20,75 @@ X33 = ["-1", "+1", "+1", "-1"]
 for i in range(4):
     print("{:<5} {:<5} {:<5} {:<5}".format(i+1,X11[i],X22[i],X33[i]))
 
-print("Матриця для m=3")
-print("{:<5} {:<5} {:<5} {:<5} {:<5} {:<5} {:<5}".format("№","X1","X2","X3","Y1","Y2","Y3"))
-X1 = [x1min, x1min, x1max, x1max]
-X2 = [x2min, x2max, x2min, x2max]
-X3 = [x3min, x3max, x3max, x3min]
-Y1 = [random.randrange(ymin,ymax, 1) for i in range(4)]
-Y2 = [random.randrange(ymin,ymax, 1) for i in range(4)]
-Y3 = [random.randrange(ymin,ymax, 1) for i in range(4)]
-for i in range(4):
-    print("{:<5} {:<5} {:<5} {:<5} {:<5} {:<5} {:<5}".format(i+1,X1[i],X2[i],X3[i],Y1[i],Y2[i],Y3[i]))
-
-y1av1 = (Y1[0]+Y2[0]+Y3[0])/3
-y2av2 = (Y1[1]+Y2[1]+Y3[1])/3
-y3av3 = (Y1[2]+Y2[2]+Y3[2])/3
-y4av4 = (Y1[3]+Y2[3]+Y3[3])/3
-
-mx1 = sum(X1)/4
-mx2 = sum(X2)/4
-mx3 = sum(X3)/4
-
-my = (y1av1 + y2av2 + y3av3 + y4av4)/4 
-
-a1 = (X1[0]*y1av1 + X1[1]*y2av2 + X1[2]*y3av3 + X1[3]*y4av4)/4
-a2 = (X2[0]*y1av1 + X2[1]*y2av2 + X2[2]*y3av3 + X2[3]*y4av4)/4
-a3 = (X3[0]*y1av1 + X3[1]*y2av2 + X3[2]*y3av3 + X3[3]*y4av4)/4
-
-a11 = (X1[0]*X1[0] + X1[1]*X1[1] + X1[2]*X1[2] + X1[3]*X1[3])/4
-a22 = (X2[0]*X2[0] + X2[1]*X2[1] + X2[2]*X2[2] + X2[3]*X2[3])/4
-a33 = (X3[0]*X3[0] + X3[1]*X3[1] + X3[2]*X3[2] + X3[3]*X3[3])/4
-a12 = a21 = (X1[0]*X2[0] + X1[1]*X2[1] + X1[2]*X2[2] + X1[3]*X2[3])/4
-a13 = a31 = (X1[0]*X3[0] + X1[1]*X3[1] + X1[2]*X3[2] + X1[3]*X3[3])/4
-a23 = a32 = (X2[0]*X3[0] + X2[1]*X3[1] + X2[2]*X3[2] + X2[3]*X3[3])/4
-
-b01 = np.array([[my, mx1, mx2, mx3], [a1, a11, a12, a13], [a2, a12, a22, a32], [a3, a13, a23, a33]])
-b02 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
-b0 = np.linalg.det(b01)/np.linalg.det(b02)
-
-b11 = np.array([[1, my, mx2, mx3], [mx1, a1, a12, a13], [mx2, a2, a22, a32], [mx3, a3, a23, a33]])
-b12 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
-b1 = np.linalg.det(b11)/np.linalg.det(b12)
-
-b21 = np.array([[1, mx1, my, mx3], [mx1, a11, a1, a13], [mx2, a12, a2, a32], [mx3, a13, a3, a33]])
-b22 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
-b2 = np.linalg.det(b21)/np.linalg.det(b22)
-
-b31 = np.array([[1, mx1, mx2, my], [mx1, a11, a12, a1], [mx2, a12, a22, a2], [mx3, a13, a23, a3]])
-b32 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
-b3 = np.linalg.det(b31)/np.linalg.det(b32)
-
-print('Запишемо отримане рівняння регресії: y = {} + ({})*x1 + ({})*x2 + ({})*x3'.format(round(b0,2), round(b1,2), round(b2,2), round(b3,2)))
-
-print("Середнє значення відгуку функції за рядками:")
-print("y1av1="+str(round(b0 + b1*X1[0] + b2*X2[0] + b3*X3[0],2))+"="+ str(round(y1av1,2)))
-print("y2av2="+str(round(b0 + b1*X1[1] + b2*X2[1] + b3*X3[1],2))+"="+ str(round(y2av2,2)))
-print("y3av3="+str(round(b0 + b1*X1[2] + b2*X2[2] + b3*X3[2],2))+"="+ str(round(y3av3,2)))
-print("y4av4="+str(round(b0 + b1*X1[3] + b2*X2[3] + b3*X3[3],2))+"="+ str(round(y4av4,2)))
-print("Значення співпадають")
-
-print("Дисперсія по рядкам:")
-d1 = ((Y1[0] - y1av1)**2 + (Y2[0] - y2av2)**2 + (Y3[0] - y3av3)**2)/3
-d2 = ((Y1[1] - y1av1)**2 + (Y2[1] - y2av2)**2 + (Y3[1] - y3av3)**2)/3
-d3 = ((Y1[2] - y1av1)**2 + (Y2[2] - y2av2)**2 + (Y3[2] - y3av3)**2)/3
-d4 = ((Y1[3] - y1av1)**2 + (Y2[3] - y2av2)**2 + (Y3[3] - y3av3)**2)/3
-print("d1=", round(d1,2),"d2=", round(d2,2),"d3=", round(d3,2),"d4=", round(d4,2))
-
-dcouple = [d1, d2, d3, d4]
-
-print("Критерій Кохрена:")
 while (True):
+    print("{:<5} {:<5} {:<5} {:<5} {:<5} {:<5} {:<5}".format("№","X1","X2","X3","Y1","Y2","Y3"))
+    X1 = [x1min, x1min, x1max, x1max]
+    X2 = [x2min, x2max, x2min, x2max]
+    X3 = [x3min, x3max, x3max, x3min]
+    Y1 = [random.randrange(ymin,ymax, 1) for i in range(4)]
+    Y2 = [random.randrange(ymin,ymax, 1) for i in range(4)]
+    Y3 = [random.randrange(ymin,ymax, 1) for i in range(4)]
+    for i in range(4):
+        print("{:<5} {:<5} {:<5} {:<5} {:<5} {:<5} {:<5}".format(i+1,X1[i],X2[i],X3[i],Y1[i],Y2[i],Y3[i]))
+
+    y1av1 = (Y1[0]+Y2[0]+Y3[0])/3
+    y2av2 = (Y1[1]+Y2[1]+Y3[1])/3
+    y3av3 = (Y1[2]+Y2[2]+Y3[2])/3
+    y4av4 = (Y1[3]+Y2[3]+Y3[3])/3
+
+    mx1 = sum(X1)/4
+    mx2 = sum(X2)/4
+    mx3 = sum(X3)/4
+
+    my = (y1av1 + y2av2 + y3av3 + y4av4)/4
+
+    a1 = (X1[0]*y1av1 + X1[1]*y2av2 + X1[2]*y3av3 + X1[3]*y4av4)/4
+    a2 = (X2[0]*y1av1 + X2[1]*y2av2 + X2[2]*y3av3 + X2[3]*y4av4)/4
+    a3 = (X3[0]*y1av1 + X3[1]*y2av2 + X3[2]*y3av3 + X3[3]*y4av4)/4
+
+    a11 = (X1[0]*X1[0] + X1[1]*X1[1] + X1[2]*X1[2] + X1[3]*X1[3])/4
+    a22 = (X2[0]*X2[0] + X2[1]*X2[1] + X2[2]*X2[2] + X2[3]*X2[3])/4
+    a33 = (X3[0]*X3[0] + X3[1]*X3[1] + X3[2]*X3[2] + X3[3]*X3[3])/4
+    a12 = a21 = (X1[0]*X2[0] + X1[1]*X2[1] + X1[2]*X2[2] + X1[3]*X2[3])/4
+    a13 = a31 = (X1[0]*X3[0] + X1[1]*X3[1] + X1[2]*X3[2] + X1[3]*X3[3])/4
+    a23 = a32 = (X2[0]*X3[0] + X2[1]*X3[1] + X2[2]*X3[2] + X2[3]*X3[3])/4
+
+    b01 = np.array([[my, mx1, mx2, mx3], [a1, a11, a12, a13], [a2, a12, a22, a32], [a3, a13, a23, a33]])
+    b02 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
+    b0 = np.linalg.det(b01)/np.linalg.det(b02)
+
+    b11 = np.array([[1, my, mx2, mx3], [mx1, a1, a12, a13], [mx2, a2, a22, a32], [mx3, a3, a23, a33]])
+    b12 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
+    b1 = np.linalg.det(b11)/np.linalg.det(b12)
+
+    b21 = np.array([[1, mx1, my, mx3], [mx1, a11, a1, a13], [mx2, a12, a2, a32], [mx3, a13, a3, a33]])
+    b22 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
+    b2 = np.linalg.det(b21)/np.linalg.det(b22)
+
+    b31 = np.array([[1, mx1, mx2, my], [mx1, a11, a12, a1], [mx2, a12, a22, a2], [mx3, a13, a23, a3]])
+    b32 = np.array([[1, mx1, mx2, mx3], [mx1, a11, a12, a13], [mx2, a12, a22, a32], [mx3, a13, a23, a33]])
+    b3 = np.linalg.det(b31)/np.linalg.det(b32)
+
+    print('Запишемо отримане рівняння регресії: y = {} + ({})*x1 + ({})*x2 + ({})*x3'.format(round(b0,2), round(b1,2), round(b2,2), round(b3,2)))
+
+    print("Середнє значення відгуку функції за рядками:")
+    print("y1av1="+str(round(b0 + b1*X1[0] + b2*X2[0] + b3*X3[0],2))+"="+ str(round(y1av1,2)))
+    print("y2av2="+str(round(b0 + b1*X1[1] + b2*X2[1] + b3*X3[1],2))+"="+ str(round(y2av2,2)))
+    print("y3av3="+str(round(b0 + b1*X1[2] + b2*X2[2] + b3*X3[2],2))+"="+ str(round(y3av3,2)))
+    print("y4av4="+str(round(b0 + b1*X1[3] + b2*X2[3] + b3*X3[3],2))+"="+ str(round(y4av4,2)))
+    print("Значення співпадають")
+
+
+
+    print("Дисперсія по рядкам:")
+    d1 = ((Y1[0] - y1av1)**2 + (Y2[0] - y2av2)**2 + (Y3[0] - y3av3)**2)/3
+    d2 = ((Y1[1] - y1av1)**2 + (Y2[1] - y2av2)**2 + (Y3[1] - y3av3)**2)/3
+    d3 = ((Y1[2] - y1av1)**2 + (Y2[2] - y2av2)**2 + (Y3[2] - y3av3)**2)/3
+    d4 = ((Y1[3] - y1av1)**2 + (Y2[3] - y2av2)**2 + (Y3[3] - y3av3)**2)/3
+    print("d1=", round(d1,2),"d2=", round(d2,2),"d3=", round(d3,2),"d4=", round(d4,2))
+
+    dcouple = [d1, d2, d3, d4]
+    print("Критерій Кохрена:")
     # Рівень значимості приймемо 0.05
     Gp = max(dcouple)/sum(dcouple)
     q = 0.05
@@ -155,5 +155,4 @@ while (True):
         break
     else:
         m+=1
-
 
